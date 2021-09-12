967e224a392c1a1a8b80b9534e912a17d8be0391
kakaheihei <78611065+kakaheihei@users.noreply.github.com>
2021-09-12 18:49:34 +0200

    week 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer, CarModel
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf, add_dealer_review_to_db
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -76,14 +76,44 @@ def registration_request(request):
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
+        dealerships = get_dealers_from_cf()
+        context = {"dealerships": dealerships}
         return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        reviews = get_dealer_reviews_from_cf(dealer_id)
+        context = {"dealer_id": dealer_id,
+                   "reviews": reviews,
+                   }
+        return render(request, 'djangoapp/dealer_details.html', context)
+
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):
+    user = request.user
+    cars = CarModel.objects.filter(dealer_id=dealer_id)
+    if request.method == "GET":
+        context = {"dealer_id": dealer_id, "cars": cars}
+        add_review_view = render(request, 'djangoapp/add_review.html', context)
 
+    if request.method == "POST":
+        form = request.POST
+        review = {
+            "name": form["name"],
+            "dealership": dealer_id,
+            "review": form["review"],
+        }
+        if form.get("purchasecheck"):
+            review["purchase"] = True
+            review["purchase_date"] = form["purchasedate"]
+            carmodel = get_object_or_404(CarModel, pk=form["car"])
+            review["car_make"] = carmodel.car_make.name
+            review["car_model"] = carmodel.name
+            review["car_year"] = carmodel.year
+        json_result = add_dealer_review_to_db(review)
+        print(json_result)
+        add_review_view = redirect('djangoapp:dealer_details', dealer_id=dealer_id)
+    return add_review_view
